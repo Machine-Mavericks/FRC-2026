832d808cd8e63a119c00dea001128d1a650c57ec
kaitlyn <cardosok@limestone.on.ca>
2026-01-22 19:51:53 -0500

    deleting everything

--- src/main/java/frc/robot/data/BuildConstants.java
+++ src/main/java/frc/robot/data/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot.data;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC-2025";
+  public static final String MAVEN_NAME = "FRC-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 205;
-  public static final String GIT_SHA = "63c3632be9bb6d2b0c4fa7bb0e282da7de7738e6";
-  public static final String GIT_DATE = "2025-04-04 21:51:01 EDT";
+  public static final int GIT_REVISION = 206;
+  public static final String GIT_SHA = "d1ed96f65ac7bfd3149688a3f552d6c38904b9cf";
+  public static final String GIT_DATE = "2025-04-15 11:19:18 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-05 10:15:50 EDT";
-  public static final long BUILD_UNIX_TIME = 1743862550201L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-22 19:21:58 EST";
+  public static final long BUILD_UNIX_TIME = 1769127718770L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
