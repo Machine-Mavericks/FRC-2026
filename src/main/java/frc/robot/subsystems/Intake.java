c6e370437cb541e3c49bcf7136baecdff8ce576e
Glenn <gbuckholtz@gmail.com>
2026-02-14 15:45:20 -0500

    i

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -12,15 +12,12 @@ public class Intake extends SubsystemBase {
     // Local objects and variables here
     // These are for things that only belong to, and used by, the subsystem
 
-<<<<<<< Updated upstream
-=======
     @Logged
-    private double commandedSpeed;
     public double currentSpeed;
+    private double commandedSpeed;
     
     private SparkMax intakeMotor;
 
->>>>>>> Stashed changes
     /** Place code here to initialize subsystem */
     public Intake() {
         SparkMax IntakeMotor = new SparkMax(2, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
