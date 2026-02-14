31d63ee735ee2f751f1947d93ddf9c1bfb126bd9
Glenn <gbuckholtz@gmail.com>
2026-02-13 19:26:45 -0500

    outlining subsystems

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+/** Subsystem */
+public class Climber extends SubsystemBase {
+
+    TalonFX climbMotor = new TalonFX(5);
+    // Local objects and variables here
+    // These are for things that only belong to, and used by, the subsystem
+
+    /** Place code here to initialize subsystem */
+    public Climber() {
+
+    }
+
+    /** Method called periodically by the scheduler
+     * Place any code here you wish to have run periodically */
+    @Override
+    public void periodic() {
+
+    }
+
+    // place special subsystem methods here
+    // this is where rest of program can access functions to return
+    // values or control the subsystem
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+/** Subsystem */
+public class TemplateSubsystem extends SubsystemBase {
+
+    // Local objects and variables here
+    // These are for things that only belong to, and used by, the subsystem
+
+    /** Place code here to initialize subsystem */
+    public TemplateSubsystem() {
+
+    }
+
+    /** Method called periodically by the scheduler
+     * Place any code here you wish to have run periodically */
+    @Override
+    public void periodic() {
+
+    }
+
+    // place special subsystem methods here
+    // this is where rest of program can access functions to return
+    // values or control the subsystem
+
+
+}
